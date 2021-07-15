78580270d7546c9b1ce5c3c2bf447798d3e7e597
Larina Fu <lrf59@cornell.edu>
2021-07-15 12:51:46 -0400

    got rid of boilerplate

--- src/App.js
+++ src/App.js
@@ -33,20 +33,6 @@ function App() {
       <Sermon isModerator={isMod} />
       <Authentication isModerator={isMod} setIsModerator={setIsMod} />
       <Footer />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
